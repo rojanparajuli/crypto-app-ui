a099e4153673c2bbd68681ffe359e57d9245dd52
rojanparajuli <rojanparajuli13@gmail.com>
2024-07-25 21:33:10 +0545

    chnages

---
+++ lib/auth/sign_in_screen.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final passwordController = TextEditingController();
+    final obscurePassword = true.obs;
+    final rememberMe = false.obs;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          Colors.green.withOpacity(0.9),
+                          BlendMode.srcATop,
+                        ),
+                        child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30)),
+                          child: Image.asset(
+                            'assets/istockphoto-478174086-612x612.jpg',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: GestureDetector(
+                            onTap: () {
+                              Get.back();
+                            },
+                            child: const Icon(
+                              Icons.arrow_back_ios_new,
+                              color: Colors.white,
+                            )),
+                      ),
+                      const Positioned(
+                        top: 50.0,
+                        left: 15.0,
+                        child: Text(
+                          "Welcome to CRYPTO",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const Positioned(
+                        top: 80.0,
+                        left: 15.0,
+                        child: Text(
+                          'Fill in the form and login to your account',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Username/E-mail',
+                  style: TextStyle(fontSize: 13),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      suffixIcon: const Icon(Icons.mail)),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Password",
+                  style: TextStyle(fontSize: 13),
+                ),
+                const SizedBox(height: 10),
+                Obx(() {
+                  return TextFormField(
+                    controller: passwordController,
+                    obscureText: obscurePassword.value,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          obscurePassword.value
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          obscurePassword.value = !obscurePassword.value;
+                        },
+                      ),
+                    ),
+                  );
+                }),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Obx(() => Checkbox(
+                          value: rememberMe.value,
+                          onChanged: (value) {
+                            rememberMe.value = value!;
+                          },
+                        )),
+                    const Text('Remember me'),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Row(
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Forgot password',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF7ed956),
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.transparent),
+                      ),
+                      padding: const EdgeInsets.all(15.0),
+                      child: const Center(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF7ed956),
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.transparent),
+                      ),
+                      padding: const EdgeInsets.all(15.0),
+                      child: const Center(
+                        child: Text(
+                          'Sign In With Fingerprint',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Don't have an Account? ",
+                      style: const TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(
+                          text: 'Sign Up',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                          ),
+                          recognizer: TapGestureRecognizer()..onTap = () {
+                            // Get.to(()=>)
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/welcome_screen.dart
+++ lib/view/welcome_screen.dart
@@ -1,4 +1,6 @@
+import 'package:crypto/auth/sign_in_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -6,81 +8,86 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Center(
-              child: Text(
-            'CRYPTO',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          )),
-          Image.asset(
-            'assets/logo.png',
-            height: 400,
-            fit: BoxFit.fitHeight,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: Center(
-              child: Text(
-                'Welcome to Crypto',
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                  child: Text(
+                'CRYPTO',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              )),
+              Image.asset(
+                'assets/logo.png',
+                height: 400,
+                fit: BoxFit.fitHeight,
               ),
-            ),
-          ),
-          const Center(child: Text('Save money by using Crypto App')),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: GestureDetector(
-              onTap: () {},
-              child: AbsorbPointer(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFF7ed956),
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(color: Colors.transparent),
-                  ),
-                  padding: const EdgeInsets.all(15.0),
-                  child: const Center(
-                    child: Text(
-                      'Sign In',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      ),
-                    ),
+              const Padding(
+                padding: EdgeInsets.all(20),
+                child: Center(
+                  child: Text(
+                    'Welcome to Crypto',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: GestureDetector(
-              onTap: () {},
-              child: AbsorbPointer(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(color: Color(0xFF7ed956)),
-                  ),
-                  padding: const EdgeInsets.all(15.0),
-                  child: const Center(
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
+              const Center(child: Text('Save money by using Crypto App')),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Get.to(() => SignInScreen());
+                  },
+                  child: AbsorbPointer(
+                    child: Container(
+                      decoration: BoxDecoration(
                         color: Color(0xFF7ed956),
-                        fontSize: 16.0,
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.transparent),
+                      ),
+                      padding: const EdgeInsets.all(15.0),
+                      child: const Center(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: AbsorbPointer(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Color(0xFF7ed956)),
+                      ),
+                      padding: const EdgeInsets.all(15.0),
+                      child: const Center(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Color(0xFF7ed956),
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
